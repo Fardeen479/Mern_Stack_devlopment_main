dba553c366bd1a015e4828d6b46b4c89294ebf3c
Fardeen Junaid <junnaidfardeen@gmail.com>
2025-05-13 17:42:27 +0600

    Add files via upload

---
+++ Switch/index.js
@@ -0,0 +1,14 @@
+var abc = prompt("Enter fruit name")
+switch (abc){
+    case "apple":
+        console.log("its apple filed")
+        document.write("its apple filed")
+        break;
+    case "orange":
+        console.log("its orange filed")
+        document.write("its orange filed")
+        break;
+    default:
+        console.log("wrong entry")
+        document.write("wrong entry")
+}
